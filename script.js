937d0016d6241b8d60a92bcd3ef63951d9caa5df
Linda Sandaker <linda.m.sandaker@gmail.com>
2022-01-05 18:48:48 +0100

    from lecture today

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
-// Today is:
-console.log("Monday");
-// What is 3*6?:
+// Declare todays day & show in console
+var d = new Date();
+console.log(d);
+// Show me the result of 3*6
 console.log(3 * 6);
 // Alert my name:
 alert("Linda");
-// Frog = animal:
+// Declare a variable, initialize frog & show in console
 var amphibian = "frog";
-// Show me frog
 console.log(amphibian);
-// Delivered = boolean -> true:
+// Declare delivered, initialize with true & show in console
 var delivered = true;
 console.log(delivered);
-// Score number = 12:
+// Declare score, initialize with 12 & show in console
 var score = 12;
 console.log(score);
-// John Smith
-var fullName = "John" + " Smith";
+// Declare fullName, join "John" "Smith".
+// Show result and type of in console.
+// Assign result with new 
+var fullName = "John" + "Smith";
 console.log(fullName);
 console.log(typeof (fullName));
 var nameType = typeof (fullName);
@@ -40,3 +42,34 @@ console.log(typeof (sumOfVariables));
 // light is on = false
 var lightIsOn = false;
 console.log(lightIsOn);
+var myNumber = 7;
+var myString = "dog";
+console.log(myNumber > 8);
+console.log(myNumber <= 8);
+console.log(myString === "dog");
+console.log(myNumber !== "cat");
+var inputIsValid = false;
+if (inputIsValid === false) {
+    alert("please fill in the email field");
+}
+
+var myAge = 29;
+if (myAge > 30) {
+    console.log("I am too old with white hair");
+}
+else if (myAge === 29) {
+    console.log("I still have a year of fun");
+}
+
+var color = "red";
+
+switch (color) {
+    case "red":
+        console.log("the chosen color is red");
+        break;
+    case "green":
+        console.log("the chosen color is green");
+        break;
+    default:
+        console.log("the color is not blue, green, red...");
+}

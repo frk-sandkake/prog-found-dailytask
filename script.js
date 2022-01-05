4f6de6e85901d51e8f7b5b4450b539a763efac09
Linda Sandaker <linda.m.sandaker@gmail.com>
2022-01-05 12:02:26 +0100

    finish daily tasks 1.2

--- script.js
+++ script.js
@@ -5,8 +5,38 @@ console.log(3 * 6);
 // Alert my name:
 alert("Linda");
 // Frog = animal:
-var animal = "frog";
+var amphibian = "frog";
+// Show me frog
+console.log(amphibian);
 // Delivered = boolean -> true:
 var delivered = true;
+console.log(delivered);
 // Score number = 12:
 var score = 12;
+console.log(score);
+// John Smith
+var fullName = "John" + " Smith";
+console.log(fullName);
+console.log(typeof (fullName));
+var nameType = typeof (fullName);
+// should be rabbit
+var animal = "rab" + "bit";
+console.log(animal);
+// string 4
+var myFirstNumber = "4";
+// number 4
+var mySecondNumber = 4;
+// sum of first and second number should be 44
+var sumOfNumbers = myFirstNumber + mySecondNumber;
+console.log(sumOfNumbers);
+// variable number 4
+var four = 4;
+// variable number 5
+var five = 5;
+// sum of variable numbers should be 9
+var sumOfVariables = four + five;
+console.log(sumOfVariables);
+console.log(typeof (sumOfVariables));
+// light is on = false
+var lightIsOn = false;
+console.log(lightIsOn);

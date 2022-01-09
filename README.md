301e6c5033ccb2bcfd1e106724f9459f80ab955c
Linda Sandaker <linda.m.sandaker@gmail.com>
2022-01-09 15:52:49 +0100

    Answers to Daily Tasks 2:1, 2:2, 2:3

--- README.md
+++ README.md
@@ -11,19 +11,49 @@ Write the following in a script.js file linked to from an index.html page. Add a
 -Create a variable called animal and assign it the string value “frog”.
 -Create a variable called delivered and assign it the boolean value true.
 -Create a variable called score and assign it the number value 12.
--Create a variable called fullName and assign it the result of joining the string values “John” and “Smith”.
-Use the "typeof" operator to find out the type of the fullName variable. Assign the result to a variable called nameType.
+-Create a variable called fullName and assign it the result of joining th string values “John” and “Smith”.
+-Use the "typeof" operator to find out the type of the fullName variable. Assign the result to a variable called nameType.
 
 1-3:
 Given the following variable declaration var age = 10; write an if-else statement that checks if age is greater than or equal to 7. If it is, console log the string “Greater than or equal to 7”. If it is not, console log the string “Less than 7”.
-
 Given the following variable declaration var animal = “cat”; write an if-else-if-if statement that checks the value of animal.
-
 Check if animal is equal to “parrot”. If it is, console log the string “The animal has two legs”.
-
 Check if animal is equal to “cat”. If it is, console log the string “The animal has four legs”.
 If animal is neither “parrot” nor “cat”, console log the string “The animal has an unknown amount of legs”. Check your code by changing the value of the animal variable.
 
+1:4
 Write a for loop that logs a number from 1 to 5
 Write a for loop that logs a number from 7 to 16
 Given this array var animals = [“dog”, “cat”, “frog”]; use a for loop to console log each animal in the array.
+
+2:1
+What is the difference between the == comparison operator and the === comparison operator?
+What would this comparison check return: “6” === 6
+What would this comparison check return: “6” == 6
+What is the difference between the != comparison operator and the !== comparison operator?
+What would this comparison check return: “4” !== 4
+What would this comparison check return: “4” != 4
+Give this variable declaration var someVar; what would this check return: typeOf(someVar);
+
+2:2
+True or false: arrays in JavaScript can hold different types of values.
+How do we determine the number of items in an array; which property can we use?
+Declare a variable called animals and assign it an empty array as an initial value.
+Add the string values “cow”, “bird” and “mouse” to the animals array using the push method.
+Use a for loop to console log every value in the animals array.
+
+2:3
+Create an array called people.
+Inside this array add two objects each containing two properties, name and age.
+Give each property and a value with an appropriate type, i.e. a string value for name and a number value for age.
+Use a for loop to log the name property of each object in the array.
+
+2:4
+Create a function called printName.
+Give the function one argument called name.
+Inside the function console log the name argument.
+Call the function and pass your name in as an argument.
+Create a function called returnName that accepts one argument called name.
+Inside the function add the string “Hello “ to the argument and return it.
+Call the function, pass in your name as the argument and assign the return value to a variable.
+Console log the variable. It should log, for example, “Hello John”.
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 // Declare todays day & show in console
 var d = new Date();
 console.log(d);
@@ -16,7 +17,7 @@ var score = 12;
 console.log(score);
 // Declare fullName, join "John" "Smith".
 // Show result and type of in console.
-// Assign result with new 
+// Assign result with new
 var fullName = "John" + "Smith";
 console.log(fullName);
 console.log(typeof (fullName));
@@ -39,6 +40,7 @@ var five = 5;
 var sumOfVariables = four + five;
 console.log(sumOfVariables);
 console.log(typeof (sumOfVariables));
+
 // From lecture 05.01.22
 // light is on = false
 var lightIsOn = false;
@@ -64,6 +66,7 @@ switch (color) {
     default:
         console.log("the color is not blue, green, red...");
 }
+
 // Declare and initialize age 10
 var age = 10;
 console.log(age);
@@ -73,6 +76,7 @@ if (age >= 7) {
 else if (age < 7) {
     console.log("Less than 7");
 }
+
 // if-else-if-if statement cat, parrot
 var animal = "cat";
 console.log(animal);
@@ -95,12 +99,12 @@ for (var i = 11; i <= 20; i++) {
 for (var i = 21; i < 30; i++) {
     console.log(i);
 }
+
 // Array is a container ["", "", ""]
 var colors = ["yellow", "green", "blue", "orange"];
 console.log(colors[0]); // show first item in array
 console.log(colors[1]); // show second item in array
 console.log(colors[2]); // show third item in array
-
 
 var firstItem = colors[0];
 console.log(firstItem);
@@ -125,18 +129,15 @@ for (var i = 0; i <= cars.length; i++) { // <= gives undefined < gives you only 
 for (var i = 0; i < cars.length; i++) { // <= gives undefined < gives you only the items
     console.log(cars[i]);
 }
-// While loop
-/*var counter = 100;
-while (counter <= 10) {
-    console.log("adding:", counter);
-    counter++;
-}*/
+// A While counter:
 var counter = 100;
 while (counter >= 10) {
     console.log("subtracting:", counter);
     counter--;
 }
+// This will go on forever!: var counter = 100;while (counter <= 10) {console.log("adding:", counter);counter++;}
 var isSubscribe = "true";
+
 // Daily tasks 1:4
 // loop 1 to 5
 for (var i = 1; i <= 5; i++) {
@@ -155,4 +156,67 @@ console.log(numberOfAnimals);
 console.log(typeof (numberOfAnimals));
 for (var i = 0; i < numberOfAnimals; i++) {
     console.log(animals[i]);
+}*/
+
+// Daily tasks 2:1
+// Comparison equals equally operator strict === and not strict ==
+var stringSix = "6";
+var numberSix = 6;
+
+if (stringSix === numberSix) { // not equal
+    console.log("equal");
+} else {
+    console.log("not equal");
+}
+if (stringSix == numberSix) { // equal - but this can creat trouble in code so always use ===
+    console.log("equal");
+} else {
+    console.log("not equal");
+}
+// Comparison not equal to operator strict !== and not strict !=
+var stringFour = "4";
+var numberFour = 4;
+
+if (stringFour !== numberFour) { // not equal
+    console.log("not equal");
+} else {
+    console.log("equal");
+}
+if (stringFour != numberFour) { // not equal
+    console.log("equal");
+} else {
+    console.log("not equal");
+}
+// Check typeof on declared variable someVar
+var someVar;
+console.log(typeof (someVar)); //undefined
+
+// Daily tasks 2:2
+// True: arrays can hold different types of values in JS
+var mixedArray = ["Linda ", 39, " has ", 2, " cats"];
+console.log(mixedArray); // Array(5) ["Linda ", 39, " has ", 2, " cats"]
+// Use the .lenth property to determine the number in the array
+console.log(mixedArray.length); // 5
+// Declare animals with an empty array
+var animals = [];
+console.log(animals); // Array []
+// Add “cow”, “bird” and “mouse” with push method
+animals.push("cow", "bird", "mouse");
+console.log(animals); // Array(3) ["cow", "bird", "mouse"]
+console.log(animals.length); // 3
+// For loop to console log all animals in the array
+for (var i = 0; i < animals.length; i++) {
+    console.log(animals[i]); // cow / bird / mouse
+}
+// Create an array of people with properties/value name/age, and for loop name
+var people = [{
+    name: "Thea",
+    age: 15
+},
+{
+    name: "Patrik",
+    age: 16
+}];
+for (var i = 0; i < people.length; i++) {
+    console.log(people[i].name); // Thea / Patrik
 }
